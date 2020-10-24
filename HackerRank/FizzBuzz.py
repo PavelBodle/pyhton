36bbe65828e496a305cbf76e6fe6dc5be61cd704
PavelBodle <paveliitb@gmail.com>
2020-10-24 14:31:07 +0530

    fizz

---
+++ HackerRank/FizzBuzz.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'fizzBuzz' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+n = 15
+def fizzBuzz(n):
+    for x in range(1, 101):
+        if x % 3 == 0 and x % 5 == 0:
+            print("FizzBuzz")
+        elif x % 3 == 0:
+            print("Fizz")
+        elif x % 5 == 0:
+            print("Buzz")
+        else:
+            print(x)
+fizzBuzz(n)
+    # Write your code here
+
+
+#if __name__ == '__main__':
